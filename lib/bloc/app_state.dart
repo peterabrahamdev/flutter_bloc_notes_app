3f16e8a8086cd8760f3ca7a9e971e959dfb1e7e9
Peter Abraham <peterabrahamdev@gmail.com>
2024-02-25 13:41:27 +0100

    feat: create app bloc states

---
+++ lib/bloc/app_state.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/foundation.dart' show immutable;
+import 'package:flutter_bloc_notes_app/models.dart';
+
+@immutable
+abstract class AppState {
+  final bool isLoading;
+  final LoginErrors? loginError;
+  final LoginHandle? loginHandle;
+  final Iterable<Note>? fetchedNotes;
+
+  const AppState.isEmpty()
+      : isLoading = false,
+        loginError = null,
+        loginHandle = null,
+        fetchedNotes = null;
+
+  const AppState({
+    required this.isLoading,
+    required this.loginError,
+    required this.loginHandle,
+    required this.fetchedNotes,
+  });
+
+  @override
+  String toString() => {
+        'isLoading': isLoading,
+        'loginError': loginError,
+        'loginHandle': loginHandle,
+        'fetchedNotes': fetchedNotes,
+      }.toString();
+}
