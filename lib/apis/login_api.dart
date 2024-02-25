9e22b6aee573fa8ff699a5bc448b6b03912c2f26
Peter Abraham <peterabrahamdev@gmail.com>
2024-02-25 12:47:00 +0100

    feat: create LoginApi

--- lib/apis/login_api.dart
+++ lib/apis/login_api.dart
@@ -11,3 +11,21 @@ abstract class LoginApiProtocol {
     required String password,
   });
 }
+
+@immutable
+class LoginApi implements LoginApiProtocol {
+  // Singleton pattern
+  const LoginApi._sharedInstance();
+  static const LoginApi _shared = LoginApi._sharedInstance();
+  factory LoginApi() => _shared;
+
+  @override
+  Future<LoginHandle?> login({
+    required String email,
+    required String password,
+  }) =>
+      Future.delayed(
+        const Duration(seconds: 2),
+        () => email == 'foo@bar.com' && password == 'foobar',
+      ).then((isLoggedIn) => isLoggedIn ? const LoginHandle.fooBar() : null);
+}
