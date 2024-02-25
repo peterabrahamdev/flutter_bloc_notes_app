8ad439124980186c8638081a82112a26731442cd
Peter Abraham <peterabrahamdev@gmail.com>
2024-02-25 16:54:07 +0100

    feat: create loading screen overlay

---
+++ lib/dialogs/loading_screen.dart
@@ -0,0 +1,112 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc_notes_app/dialogs/loading_screen_controller.dart';
+
+class LoadingScreen {
+  LoadingScreen._sharedInstance();
+  static final _shared = LoadingScreen._sharedInstance();
+  factory LoadingScreen.instance() => _shared;
+
+  LoadingScreenController? _controller;
+
+  void show({
+    required BuildContext context,
+    required String text,
+  }) {
+    // Controller can be null and if that's the case, we take its value on the left side (null) otherwise it's false
+    if (_controller?.update(text) ?? false) {
+      return;
+    } else {
+      _showOverlay(
+        context: context,
+        text: text,
+      );
+    }
+  }
+
+  void hide() {
+    // If we have a controller available, then close it. After we assign a value null to it.
+    _controller?.close();
+    _controller = null;
+  }
+
+  LoadingScreenController _showOverlay({
+    required BuildContext context,
+    required String text,
+  }) {
+    final _text = StreamController<String>();
+    _text.add(text);
+    // get the size of the screen
+    final state = Overlay.of(context);
+    final renderBox = context.findRenderObject() as RenderBox;
+    final size = renderBox.size;
+
+    final overlay = OverlayEntry(
+      builder: (context) {
+        return Material(
+          color: Colors.black.withAlpha(150),
+          child: Center(
+            child: Container(
+              constraints: BoxConstraints(
+                maxHeight: size.height * 0.8,
+                maxWidth: size.width * 0.8,
+                minWidth: size.width * 0.5,
+              ),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: SingleChildScrollView(
+                    child: Column(
+                  // We need to set this in order to prevent the box from being elongated to the top and bottom of the screen.
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const CircularProgressIndicator(),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    StreamBuilder(
+                      stream: _text.stream,
+                      builder: (context, snapshot) {
+                        if (snapshot.hasData) {
+                          return Text(
+                            snapshot.data!,
+                            textAlign: TextAlign.center,
+                          );
+                        } else {
+                          return Container();
+                        }
+                      },
+                    ),
+                  ],
+                )),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+    // We insert the created overlay on the screen.
+    state.insert(overlay);
+
+    // Creating the LoadingScreenController in order to implement the two action functions.
+    return LoadingScreenController(
+      close: () {
+        _text.close();
+        overlay.remove();
+        return true;
+      },
+      update: (text) {
+        _text.add(text);
+        return true;
+      },
+    );
+  }
+}
