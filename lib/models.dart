d9b9ae744de69ddf4868f96329104372461f4e22
Peter Abraham <peterabrahamdev@gmail.com>
2024-02-25 12:59:02 +0100

    docs: add documentation comments to models

--- lib/models.dart
+++ lib/models.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/foundation.dart' show immutable;
 
+/// Represents a handle obtained after successful login.
+
+/// An immutable class that holds a login token.
+
 @immutable
 class LoginHandle {
+  /// The login token.
+
   final String token;
+
+  /// Creates a new `LoginHandle` instance.
 
   const LoginHandle({
     required this.token,
   });
+
+  /// Creates a `LoginHandle` instance with the token "foobar".
 
   const LoginHandle.fooBar() : token = 'foobar';
 
@@ -20,12 +30,24 @@ class LoginHandle {
   String toString() => 'LoginHandle (token = $token)';
 }
 
-enum LoginErrors { invalidHandle }
+/// Represents different types of errors related to LoginHandle operations.
 
+enum LoginErrors {
+  /// Error indicating an invalid LoginHandle object.
+  invalidHandle
+}
+
+/// Represents a single note with a title.
+
+/// An immutable class that holds a title of a note.
 @immutable
 class Note {
+  /// The title of the note.
+
   final String title;
 
+  /// Creates a new `Note` instance.
+  
   const Note({
     required this.title,
   });
@@ -33,6 +55,8 @@ class Note {
   @override
   String toString() => 'Note (title = $title)';
 }
+
+/// A list of mock notes for testing purposes.
 
 final mockNotes = Iterable.generate(
   3,
