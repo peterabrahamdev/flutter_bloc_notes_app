735abaa9cb31c0c64f72f97b39ca3ee2f3ec74e9
Peter Abraham <peterabrahamdev@gmail.com>
2024-02-28 12:18:23 +0100

    chore: add collection import to app bloc

--- lib/bloc/app_bloc.dart
+++ lib/bloc/app_bloc.dart
@@ -1,3 +1,4 @@
+import 'package:collection/collection.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/foundation.dart' show immutable;
 import 'package:flutter_bloc_notes_app/apis/login_api.dart';
