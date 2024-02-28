befefc998221e7f110203b7ce4bb4bdbacb6d216
Peter Abraham <peterabrahamdev@gmail.com>
2024-02-28 10:19:13 +0100

    feat: add bloc consumer to home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_notes_app/apis/login_api.dart';
+import 'package:flutter_bloc_notes_app/apis/notes_api.dart';
+import 'package:flutter_bloc_notes_app/bloc/app_bloc.dart';
+import 'package:flutter_bloc_notes_app/dialogs/generic_dialog.dart';
+import 'package:flutter_bloc_notes_app/dialogs/loading_screen.dart';
+import 'package:flutter_bloc_notes_app/models.dart';
+import 'package:flutter_bloc_notes_app/screens/iterable_list_screen.dart';
+import 'package:flutter_bloc_notes_app/screens/login_screen.dart';
+import 'package:flutter_bloc_notes_app/strings.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,60 +19,60 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return BlocProvider(
+      create: (context) => AppBloc(
+        loginApi: LoginApi(),
+        notesApi: const NotesApi(),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      child: MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text(homePage),
+          ),
+          body: BlocConsumer<AppBloc, AppState>(
+            listener: (context, state) {
+              // loading screen
+              if (state.isLoading) {
+                LoadingScreen.instance().show(
+                  context: context,
+                  text: pleaseWait,
+                );
+              } else {
+                LoadingScreen.instance().hide();
+              }
+              // display possible errors
+              if (state.loginError != null) {
+                showGenericDialog<bool>(
+                  context: context,
+                  title: loginErrorDialogTitle,
+                  content: loginErrorDialogContent,
+                  optionsBuilder: () => {ok: true},
+                );
+              }
+              // if we are logged in, but we have no fetched notes, fetch them now
+              if (state.isLoading == false &&
+                  state.loginError == null &&
+                  state.loginHandle == const LoginHandle.fooBar() &&
+                  state.fetchedNotes == null) {
+                context.read<AppBloc>().add(const LoadNotesEvent());
+              }
+            },
+            builder: (context, state) {
+              final notes = state.fetchedNotes;
+              if (notes == null) {
+                return LoginScreen(
+                  onLoginTapped: (email, password) {
+                    context.read<AppBloc>().add(
+                          LoginEvent(email: email, password: password),
+                        );
+                  },
+                );
+              } else {
+                return notes.toListView();
+              }
+            },
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
